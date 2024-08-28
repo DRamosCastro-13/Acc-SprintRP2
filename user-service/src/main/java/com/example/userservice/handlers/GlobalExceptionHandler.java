5d99c2d90e32120875abc7e1103526fc281b50d0
danieloka <danirc01@gmail.com>
2024-08-28 18:54:44 -0500

    logger included and exception handler, missing some validations

--- user-service/src/main/java/com/example/userservice/handlers/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/example/userservice/handlers/GlobalExceptionHandler.java
@@ -1,16 +1,24 @@
 package com.example.userservice.handlers;
 
+import com.example.userservice.services.Impl.UserEntityServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import reactor.core.publisher.Mono;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler({NotFoundException.class})
     public Mono<ResponseEntity<String>> handlerNotFoundException( NotFoundException ex ){
+        logger.error("Exception: {}. Message: {}",
+              ex.getClass().getSimpleName(), ex.getMessage());
         return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage()));
     }
 }
--- user-service/src/main/java/com/example/userservice/services/Impl/UserEntityServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/services/Impl/UserEntityServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class UserEntityServiceImpl implements UserEntityService {
@@ -16,14 +18,20 @@ public class UserEntityServiceImpl implements UserEntityService {
     @Autowired
     private UserEntityRepository userEntityRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserEntityServiceImpl.class);
+
     @Override
     public Mono<UserEntity> getUserById(Long id) {
-        return userEntityRepository.findById(id).switchIfEmpty( Mono.error( new NotFoundException("User not found")) );
+        return userEntityRepository.findById(id).switchIfEmpty( Mono.error( new NotFoundException("User not found")));
     }
 
     @Override
     public Flux<UserEntity> getAllUsers() {
-        return userEntityRepository.findAll().switchIfEmpty( Mono.error(new NotFoundException("No users found")));
+        return userEntityRepository.findAll()
+                .doOnSubscribe(subscription -> logger.info("Request to retrieve all users")) // cuando alguien se suscribe se ejecua
+                .doOnNext(user -> logger.debug("Retrieved user: {}", user.getName())) // por cada elemento del flux se ejecuta
+                .doOnError(error -> logger.error("Error retrieving users: {}", error.getMessage())) // en caso de error se ejecuta
+                .doOnComplete(() -> logger.info("Completed retrieving all users")); // cuando se complete la petición
     }
 
 
